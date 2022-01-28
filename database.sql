2d2a26b77c531cea029225f68743b2c644d3b1c6
Zane Klausing <zkla1998@gmail.com>
2022-01-28 10:39:11 -0600

    added database code to database.sql

--- database.sql
+++ database.sql
@@ -3,8 +3,33 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+CREATE TABLE “public.user” (
+	“id” serial PRIMARY KEY,
+	“username” varchar(255) UNIQUE NOT NULL,
+	“password” varchar(255) NOT NULL,
+	“sec_level” integer NOT NULL,
+	“avail_start” DATE NOT NULL,
+	“avail_end” DATE NOT NULL,
+	“flight_event_id” integer NOT NULL,
+	“signed_contract” BOOLEAN NOT NULL,
+	“paid” BOOLEAN NOT NULL,
+	“covid_free” BOOLEAN NOT NULL,
+	“in_group” BOOLEAN NOT NULL,
+	“flight_direction” VARCHAR(4) NOT NULL,
+	CONSTRAINT “user_pk” PRIMARY KEY (“id”)
+);
+CREATE TABLE “public.flight event” (
+	“id” serial NOT NULL,
+	“date” serial NOT NULL,
+	“nyc team leader” integer NOT NULL,
+	“lon team leader” integer NOT NULL,
+	CONSTRAINT “flight event_pk” PRIMARY KEY (“id”)
+);
+CREATE TABLE “public.pet” (
+	“id” serial NOT NULL,
+	“name” varchar(255) NOT NULL,
+	“breed” varchar(255) NOT NULL,
+	“owner_id” serial NOT NULL,
+	“weight” integer(255) NOT NULL,
+	CONSTRAINT “pet_pk” PRIMARY KEY (“id”)
 );
