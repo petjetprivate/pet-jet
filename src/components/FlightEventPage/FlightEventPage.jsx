f863c7fe1ef2a929f2150cc46237705e54cf7890
Adam Iverson <aciverso@gmail.com>
2022-02-03 16:19:14 -0600

    sticky table css turned off, dummy data inserted into database.sql

--- src/components/FlightEventPage/FlightEventPage.jsx
+++ src/components/FlightEventPage/FlightEventPage.jsx
@@ -91,15 +91,15 @@ function FlightEventPage() {
 
     // let el = document.getElementById(e.target.value);
     // console.log('e.target.value:*********', e.target.value);
-      // if (el.className === "hidden") {
-      //   el.className = "block";
-      // } else {
-      //   el.className = "hidden";
-      // }
+    // if (el.className === "hidden") {
+    //   el.className = "block";
+    // } else {
+    //   el.className = "hidden";
+    // }
   };
 
   return (
-    <div>
+    <div className="fixTableHead">
       <div>
         <h1>Flight Event Page</h1>
         <div className="flexbox neighbors">
@@ -154,27 +154,28 @@ function FlightEventPage() {
           />
           <button type="submit">Submit</button>
         </form>
-        <p>Passengers go here:</p>
+        <p>Passengers:</p>
+        <div>
         <table>
           <thead>
-          <tr>
-            <th>Username</th>
-            <th>Full Name</th>
-            <th>Phone Number</th>
-            <th>Email</th>
-            <th>Availability Start</th>
-            <th>Availability End</th>
-            <th>Flight Event</th>
-            <th>Contract Signed</th>
-            <th>Paid</th>
-            <th>COVID Ready</th>
-            <th>Continent of Origin</th>
-          </tr>
+            <tr>
+              <th>Username</th>
+              <th>Full Name</th>
+              <th>Phone Number</th>
+              <th>Email</th>
+              <th>Availability Start</th>
+              <th>Availability End</th>
+              <th>Flight Event</th>
+              <th>Continent of Origin</th>
+              <th>Contract Signed</th>
+              <th>Paid</th>
+              <th>COVID Ready</th>
+            </tr>
           </thead>
           <tbody>
-          {passengers.map((passenger) => {
-            return (
-              <tr key={passenger.id}>
+            {passengers.map((passenger) => {
+              return (
+                <tr key={passenger.id}>
                   <td>{passenger.username}</td>
                   <td>{passenger.full_name}</td>
                   <td>{passenger.phone_num}</td>
@@ -182,15 +183,38 @@ function FlightEventPage() {
                   <td>{passenger.avail_start}</td>
                   <td>{passenger.avail_end}</td>
                   <td>{passenger.flight_event_id}</td>
-                  <td>{passenger.signed_contract}</td>
-                  <td>{passenger.covid_free}</td>
-                  <td>{passenger.paid}</td>
-                  <td>{passenger.continent_origin}</td>
-              </tr>
-            );
-          })}
+                  <td>
+                    {passenger.continent_origin}
+                  </td>
+                  <td>
+                    <label htmlFor="contract">
+                      <input
+                        type="checkbox"
+                        name="contract"
+                        value="signed_contract"
+                      />
+                    </label>
+                  </td>
+                  <td>
+                    <input 
+                      type="checkbox" 
+                      name="paid" 
+                      value="paid" 
+                      />
+                  </td>
+                  <td>
+                    <input 
+                      type="checkbox"
+                      name="covid_free"
+                      value="covid_free"
+                      />
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
+        </div>
       </div>
     </div>
   );
