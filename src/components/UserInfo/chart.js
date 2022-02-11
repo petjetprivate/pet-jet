fa185c757cd17fbfdae4434bf318d75ca2726c8f
Jerome-Horton <riqui.howard@gmail.com>
2022-02-11 00:37:54 -0600

    imported chart reducer and dispatched in useEffect

--- src/components/UserInfo/chart.js
+++ src/components/UserInfo/chart.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState} from "react";
 import Chart from 'chart.js/auto'
 import { Line } from 'react-chartjs-2';
 import './chart.css';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 // Test Chart for configuration
 
 
 function flightChart() {
 
-    // import all passengers flight dates for departure and return useSelector
-    // const user = useSelector(store => store.user)
-    // link it to data to show demand in chart
+    const dispatch = useDispatch();
+    const chart = useSelector(store => store.chart)
+
+    useEffect (() => {
+        dispatch({
+            type: "GET_CHARTS_INFO"
+        })
+    }, []);
+
 
     const data = {
         labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"],
         datasets: [
             {
-                label: "Departure Dates",
+                label: "United States Flight Demand",
                 data: [33, 53, 85, 41, 44, 65, 23, 56, 75, 60, 38, 49],
                 fill: true,
                 backgroundColor: "rgba(75,192,192,0.2)",
                 borderColor: "rgba(75,192,192,1)"
             },
             {
-                label: "Return Dates",
+                label: "Europe Flight Demand",
                 data: [33, 25, 45, 51, 54, 76, 33, 65, 40, 64, 70, 80 ],
                 fill: false,
                 borderColor: "#742774"
