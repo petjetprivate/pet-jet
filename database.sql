5a4110f26df093b0470d5630018fc1b9f9c67642
Adam Iverson <aciverso@gmail.com>
2022-02-16 10:54:09 -0600

    database.sql finalized

--- database.sql
+++ database.sql
@@ -4,10 +4,7 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
+DROP TABLE IF EXISTS "user";
 CREATE TABLE "user" (
 	"id" serial PRIMARY KEY,
 	"username" varchar(255) UNIQUE NOT NULL,
@@ -19,12 +16,13 @@ CREATE TABLE "user" (
 	"avail_start" DATE DEFAULT NULL,
 	"avail_end" DATE DEFAULT NULL,
 	"flight_event_id" integer DEFAULT NULL,
-	"signed_contract" BOOLEAN DEFAULT NULL,
-	"paid" BOOLEAN DEFAULT NULL,
-	"covid_free" BOOLEAN DEFAULT NULL,
+	"signed_contract" BOOLEAN DEFAULT FALSE,
+	"paid" BOOLEAN DEFAULT FALSE,
+	"covid_free" BOOLEAN DEFAULT FALSE,
 	"continent_origin" VARCHAR(2) NOT NULL
 );
 
+DROP TABLE IF EXISTS "flight_event";
 CREATE TABLE "flight_event" (
 	"id" serial PRIMARY KEY,
 	"name" varchar(255) NOT NULL,
@@ -34,6 +32,7 @@ CREATE TABLE "flight_event" (
 	"EUTeamLead" integer DEFAULT NULL
 );
 
+DROP TABLE IF EXISTS "pet";
 CREATE TABLE "pet" (
 	"id" serial PRIMARY KEY,
 	"name" varchar(255) NOT NULL,
@@ -44,17 +43,6 @@ CREATE TABLE "pet" (
 
 INSERT INTO "public"."user"("username", "password", "full_name", "phone_num", "email", "sec_level", "avail_start", "avail_end", "continent_origin") 
 VALUES('AdminRose', '$2a$10$TZ3rW70oJIgNQ/2ulfsutuuotLCEo42USxgfiSBxSqVPf6gqq2jdG', 'Rose', '6128005555', 'rose@horns.com', '2', '2022-10-29', '2022-10-31', 'US'), 
-('Joe', '$2a$10$TZ3rW70oJIgNQ/2ulfsutuuotLCEo42USxgfiSBxSqVPf6gqq2jdG', 'Joseph', '5128806000', 'joe@joe.com', '2',  '2022-09-17', '2022-09-30', 'EU'),
-('Mary', '$2a$10$TZ3rW70oJIgNQ/2ulfsutuuotLCEo42USxgfiSBxSqVPf6gqq2jdG', 'Marietta', '5559990000', 'marietta@georgia.gov', '1', '2022-03-19', '2022-11-11', 'US'),
-('Malcolm', '$2a$10$TZ3rW70oJIgNQ/2ulfsutuuotLCEo42USxgfiSBxSqVPf6gqq2jdG', 'Malcolm', '55554444', 'little@middle.com', '0', '2022-05-01', '2023-05-31', 'US'),
-('OmahaTodd', '$2a$10$TZ3rW70oJIgNQ/2ulfsutuuotLCEo42USxgfiSBxSqVPf6gqq2jdG', 'Toddrick von Waddingham', '1234441233', 'omaha@todd.com', '0', '2022-04-11', '2022-12-31', 'EU'),
-('Denice', '$2a$10$TZ3rW70oJIgNQ/2ulfsutuuotLCEo42USxgfiSBxSqVPf6gqq2jdG', 'Denice Power', '6053526834', 'denice@knees.com', '2022-03-17', '2023-03-17', '0', 'EU'),
-('WhiskyBill', '$2a$10$TZ3rW70oJIgNQ/2ulfsutuuotLCEo42USxgfiSBxSqVPf6gqq2jdG', 'Whisker Billiam', '6059521619', 'fan@twins.com', '0', '2022-09-01', '2022-10-20', 'US'),
-('Jerome', '$2a$10$TZ3rW70oJIgNQ/2ulfsutuuotLCEo42USxgfiSBxSqVPf6gqq2jdG', 'Jerome Horton', '6080059944', 'jerome@horton.com', '2', '2022-04-04', '2022-04-30', 'US'),
-('Amber', '$2a$10$TZ3rW70oJIgNQ/2ulfsutuuotLCEo42USxgfiSBxSqVPf6gqq2jdG', 'Amber Sorenson', '9208785656', 'amber@sorenson.com', '2', '2022-08-01', '2022-08-31', 'US'),
-('Zane', '$2a$10$TZ3rW70oJIgNQ/2ulfsutuuotLCEo42USxgfiSBxSqVPf6gqq2jdG', 'Zane Klausing', '888300033', 'zane@klausing.com', '2', '2023-01-01', '2023-03-31', 'US'),
-('TheADog', '$2a$10$TZ3rW70oJIgNQ/2ulfsutuuotLCEo42USxgfiSBxSqVPf6gqq2jdG', 'Adam Iverson', '5123528716', 'adam@iverson.com', '2', '2022-03-01', '2022-05-31', 'US'),
-('Trooms', '$2a$10$TZ3rW70oJIgNQ/2ulfsutuuotLCEo42USxgfiSBxSqVPf6gqq2jdG', 'Harry S. Truman', '1800333444', 'harry@truman.com', '0', '2022-02-14', '2024-01-01', 'US')
 RETURNING "username", "password", "full_name", "sec_level", "avail_start", "avail_end", "flight_event_id", "signed_contract", "paid", "covid_free", "continent_origin";
 
 
